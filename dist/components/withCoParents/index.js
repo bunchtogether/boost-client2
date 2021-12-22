ad4450e34b1cbf62b46a7ddde50c0c308fc9ea3c
John Wehr <johnwehr@bunchenterprise.com>
2021-12-22 18:14:58 -0500

    Add withCoParent HOC

---
+++ dist/components/withCoParents/index.js
@@ -0,0 +1,124 @@
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+import * as React from 'react';
+import isEmpty from 'lodash/isEmpty';
+import pick from 'lodash/pick';
+import omit from 'lodash/omit';
+import queryString from 'query-string';
+import hoistNonReactStatics from 'hoist-non-react-statics';
+import { cachedValue, cachedSubscribe, cachedUnsubscribe } from '../../index';
+import { logSubscribeError } from '../lib/error-logging';
+const parameterNames = new Set(['type', 'childType', 'sort', 'order', 'limit', 'offset', 'filter', 'query', 'hasGrandchild', 'coparentEdgeContains', 'edgeContains']);
+
+const getParameters = (...args) => pick(Object.assign({}, ...args), [...parameterNames]);
+
+export default ((parameters = {}) => function wrap(Component) {
+  const getName = props => {
+    const id = parameters.idName ? props[parameters.idName] : props.id;
+
+    if (!id) {
+      return undefined;
+    }
+
+    const options = getParameters(parameters, props);
+
+    if (isEmpty(options)) {
+      return `n/${id}/coparents`;
+    }
+
+    return `n/${id}/coparents?${queryString.stringify(options)}`;
+  };
+
+  class NewComponent extends React.Component {
+    static getDerivedStateFromProps(nextProps, prevState) {
+      const name = getName(nextProps);
+
+      if (name !== prevState.name) {
+        return {
+          name,
+          coparents: cachedValue(name)
+        };
+      }
+
+      return null;
+    }
+
+    constructor(props) {
+      super(props);
+
+      _defineProperty(this, "handleUpdate", value => {
+        this.setState({
+          coparents: value
+        });
+      });
+
+      _defineProperty(this, "handleError", error => {
+        logSubscribeError(this.state.name, error);
+      });
+
+      const name = getName(props);
+      this.state = {
+        name,
+        coparents: cachedValue(name)
+      };
+    }
+
+    async componentDidMount() {
+      if (this.state.name) {
+        cachedSubscribe(this.state.name, this.handleUpdate, this.handleError);
+      }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+      if (this.state.name !== nextState.name) {
+        const name = this.state.name;
+
+        if (name) {
+          cachedUnsubscribe(name, this.handleUpdate, this.handleError);
+        }
+
+        if (nextState.name) {
+          cachedSubscribe(nextState.name, this.handleUpdate, this.handleError);
+        }
+      }
+
+      if (this.state.coparents !== nextState.coparents) {
+        return true;
+      }
+
+      const nextPropsKeys = Object.keys(nextProps).filter(key => !parameterNames.has(key));
+      const propsKeys = Object.keys(this.props).filter(key => !parameterNames.has(key));
+
+      if (nextPropsKeys.length !== propsKeys.length) {
+        return true;
+      }
+
+      for (let i = 0; i < propsKeys.length; i += 1) {
+        const key = propsKeys[i];
+
+        if (nextProps[key] !== this.props[key]) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    async componentWillUnmount() {
+      if (this.state.name) {
+        cachedUnsubscribe(this.state.name, this.handleUpdate, this.handleError);
+      }
+    }
+
+    render() {
+      const props = omit(this.props, [...parameterNames]);
+      props[parameters.propertyName || 'coparents'] = this.state.coparents;
+      return /*#__PURE__*/React.createElement(Component, props);
+    }
+
+  }
+
+  hoistNonReactStatics(NewComponent, Component);
+  return NewComponent;
+});
+//# sourceMappingURL=index.js.map
---
+++ src/components/withCoParents/index.jsx
@@ -0,0 +1,145 @@
+// @flow
+
+import type { List } from 'immutable';
+import * as React from 'react';
+import isEmpty from 'lodash/isEmpty';
+import pick from 'lodash/pick';
+import omit from 'lodash/omit';
+import queryString from 'query-string';
+import hoistNonReactStatics from 'hoist-non-react-statics';
+import { cachedValue, cachedSubscribe, cachedUnsubscribe } from '../../index';
+import { logSubscribeError } from '../lib/error-logging';
+
+const parameterNames = new Set([
+  'type',
+  'childType',
+  'sort',
+  'order',
+  'limit',
+  'offset',
+  'filter',
+  'query',
+  'hasGrandchild',
+  'coparentEdgeContains',
+  'edgeContains',
+]);
+
+const getParameters = (...args:Array<Object>):Parameters => pick(Object.assign({}, ...args), [...parameterNames]);
+
+type Parameters = {
+  type?: string,
+  childType?: string,
+  sort?: string,
+  order?: string,
+  limit?: number,
+  offset?: number,
+  filter?: Object,
+  query?: string,
+  hasGrandchild?: string,
+  coparentEdgeContains?: string,
+  edgeContains?: string,
+  propertyName?: string,
+  idName?: string,
+};
+
+type State = {
+  name?: string,
+  coparents?: List<string>
+};
+
+export default (parameters: Parameters = {}) => function wrap<Props: Object>(Component: React.AbstractComponent<Props>): React.AbstractComponent<$Diff<Props, { [string]: List<string> }>> {
+  const getName = (props: Props) => {
+    const id = parameters.idName ? props[parameters.idName] : props.id;
+    if (!id) {
+      return undefined;
+    }
+    const options = getParameters(parameters, props);
+    if (isEmpty(options)) {
+      return `n/${id}/coparents`;
+    }
+    return `n/${id}/coparents?${queryString.stringify(options)}`;
+  };
+
+  class NewComponent extends React.Component<Props, State> {
+    static getDerivedStateFromProps(nextProps: Props, prevState: State) {
+      const name = getName(nextProps);
+      if (name !== prevState.name) {
+        return {
+          name,
+          coparents: cachedValue(name),
+        };
+      }
+      return null;
+    }
+
+    constructor(props: Props) {
+      super(props);
+      const name = getName(props);
+      this.state = {
+        name,
+        coparents: cachedValue(name),
+      };
+    }
+
+    async componentDidMount() {
+      if (this.state.name) {
+        cachedSubscribe(this.state.name, this.handleUpdate, this.handleError);
+      }
+    }
+
+    shouldComponentUpdate(nextProps:Props, nextState:State) {
+      if (this.state.name !== nextState.name) {
+        const name = this.state.name;
+        if (name) {
+          cachedUnsubscribe(name, this.handleUpdate, this.handleError);
+        }
+        if (nextState.name) {
+          cachedSubscribe(nextState.name, this.handleUpdate, this.handleError);
+        }
+      }
+      if (this.state.coparents !== nextState.coparents) {
+        return true;
+      }
+      const nextPropsKeys = Object.keys(nextProps).filter((key) => !parameterNames.has(key));
+      const propsKeys = Object.keys(this.props).filter((key) => !parameterNames.has(key));
+      if (nextPropsKeys.length !== propsKeys.length) {
+        return true;
+      }
+      for (let i = 0; i < propsKeys.length; i += 1) {
+        const key = propsKeys[i];
+        if (nextProps[key] !== this.props[key]) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    async componentWillUnmount() {
+      if (this.state.name) {
+        cachedUnsubscribe(this.state.name, this.handleUpdate, this.handleError);
+      }
+    }
+
+
+    handleUpdate = (value:any) => {
+      this.setState({
+        coparents: value,
+      });
+    }
+
+    handleError = (error:Error) => {
+      logSubscribeError(this.state.name, error);
+    }
+
+    render() {
+      const props = omit(this.props, [...parameterNames]);
+      props[parameters.propertyName || 'coparents'] = this.state.coparents;
+      return <Component {...props} />;
+    }
+  }
+
+  hoistNonReactStatics(NewComponent, Component);
+
+  return NewComponent;
+};
+
